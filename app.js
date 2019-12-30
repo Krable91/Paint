bd120636ec1841245f5264f35c31d8525f1d6051
krable91 <dydansp@naver.com>
2019-12-31 02:55:34 +0900

    Color Changed

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColors");
+
+/*Array.from(colors).forEach(function(color){
+    return color.addEventListener('click');
+}, );*/
+Array.from(colors).forEach(item => item.addEventListener('click', changeColor));
+canvas.width = 700;
+canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -17,7 +25,7 @@ function startPainting(){
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){
+    if(painting === false){
         ctx.beginPath();
         ctx.moveTo(x, y);
     } else {
@@ -26,11 +34,11 @@ function onMouseMove(event) {
     }
     //console.log(x, y);
 }
-function onMouseDown(event){
-    painting = true;
-    //console.log(event);
+function changeColor(){
+    const color = event.target.style.backgroundColor;
+    console.log(color);
+    ctx.strokeStyle = color;
 }
-
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
